69a29a807468738a7d27648b9a78fb0c850a714e
Fedor <fordin2020@gmail.com>
2022-04-22 19:02:41 +0300

    added: add to cart button

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import './App.css';
 import SideBar from './SideBar';
 import Store from './Store';
 import products from './products';
-function App() : JSX.Element{
-  return (
-    <div className="App">
-      <Header></Header>
-      <Store className="Store" products={products}></Store>
-    </div>
-  );
+import { Component, useRef} from 'react';
+import Product from './interfaces';
+import Cart from "./Cart"
+class App extends Component<{}, {products:Array<Product>}> {
+
+  constructor(props : any){
+    super(props);
+    this.state = {products: []}
+
+  }
+
+  addProduct = (product : Product) => {
+    
+    let newArray : Array<Product> = this.state.products
+    newArray.push(product)
+    this.setState({products: newArray})
+ 
+  }
+  render() {return (
+      <div className="App">
+        <Header products={this.state.products}></Header>
+        <Cart products={this.state.products}></Cart>
+        <Store addProduct={this.addProduct} className="Store" products={products}></Store>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Store.tsx
+++ src/Store.tsx
@@ -1,24 +1,39 @@
 import "./Store.css"
 import Product from "./interfaces"
-
+import {Component} from "react"
 // эта функция отрисовывает из переданного ей объекта товары ввиде карточек
-function Store(props : any) : JSX.Element {
-   // if (typeof props.products === "Product"){return <h1>TYPE ERROR</h1>} 
-  
-    let toRender : Array<JSX.Element> = [];  
-    props.products.forEach((product : Product, index : number) => {
 
-        if (product.name.length > 12 ) return // чтобы имя всегда влезало на строку
+class Store extends Component<{className : string, products : Array<Product>, addProduct : (arg0:Product) => void}, {products : Product[]}> {
+    constructor(props : any) {
+        super(props);
+        this.state = {products: props.products};
         
-        toRender.push(
-        <div key={index} className="productBody">
-            <h2 className="name">{product.name} </h2>
-            <h2 className="price">{product.price + " $"}</h2>
-            <button className="buyButton">Купить</button>
-            <img  className="productImg" src={product.img}/>
-        </div>)
-    })
-   
-    return(<div className={props.className}>{toRender}</div>);
+    }
+    addProduct(index : number) : () => void{
+        let id : number= index
+        return () => {
+
+            this.props.addProduct(this.state.products[id])
+        }
+    }
+
+    render(){
+        let toRender : Array<JSX.Element> = [];  
+        this.state.products.forEach((product : Product, index : number) => {
+    
+            if (product.name.length > 12 ) return // чтобы имя всегда влезало на строку
+            
+            toRender.push(
+            <div key={index} className="productBody">
+                <h2 className="name">{product.name} </h2>
+                <h2 className="price">{product.price + " $"}</h2>
+                <button className="buyButton" onClick={this.addProduct(index)}>Купить</button>
+                <img  className="productImg" src={product.img}/>
+            </div>)
+        })
+       
+        return(<div className={this.props.className}>{toRender}</div>);
+    }
 }
 export default Store;
+
